5a484574dff0552bff99b19eafdc9c8e648aa74b
Kace at Telematique <kaceong@gmail.com>
2024-03-14 13:21:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Flutter / Supabase Production Template for Startups
+
+**Demo**: [https://app.devtodollars.com](https://app.devtodollars.com)
+
+**Docs**: [See Pricing](https://devtodollars.com/#pricing)
+
+---
+
+### Frontend ([Flutter](https://flutter.dev/))
+
+* State Management ([riverpod](https://pub.dev/packages/riverpod))
+* Routing ([go\_router](https://pub.dev/packages/go\_router))
+* UI Includes:
+  * auth\_screen
+  * payments\_screen (to redirect to stripe)
+  * home\_screen
+* 🚧 [frontend tests](https://github.com/devtodollars/flutter-supabase-production-template/issues/4) 🚧
+
+### Backend ([Supabase](https://supabase.com/))
+
+* Authentication
+  * Email + PW
+  * SSO (Google, Github, etc.)
+* Fully configured for local development from day one
+* 🚧 [backend tests](https://github.com/devtodollars/flutter-supabase-production-template/issues/16) 🚧
+
+### Analytics ([Posthog](https://posthog.com/))
+
+* Unified analytics across frontend and backend based on `user_id`
+  * Frontend analytics comes pre-installed for iOS, Android, Web, and MacOS
+  * Backend analytics installed and linked to frontend analytics
+* Basic events captured:
+  * `user signs in`
+  * `user signs up`
+  * `user deletes account`
+  * `user starts checkout`
+  * `user opens billing portal`
+  * `user completes checkout`
+
+### Payments ([Stripe](https://stripe.com/en-ca))
+
+* Stripe fully setup and works with one-time payments and subscriptions
+  * `stripe` table to store `stripe_customer_id` and current `active_products`
+  * Stripe webhook to sync user subscriptions from stripe to supabase
+  * Deno function to retrieve the billing portal url or a checkout session url which works with the `payments_screen`
+
+### Release ([Github Actions](https://github.com/features/actions))
+
+* Script to `bumpversion.sh` following semantic versioning
+* Deploys a web preview on Netlify for every PR
+* Publishing pipeline to publish to Netlify and to publish supabase functions
+* 🚧 [Workflow to create signed releases for Android and iOS](https://github.com/devtodollars/flutter-supabase-production-template/issues/22) 🚧
+
+### Emails ([Postmark](https://postmarkapp.com/))
+
+* Send transactional emails
+* DNS setup to avoid spam folder (DKIM, DMARC, SPF in subdomain)
+* Simplify email updates by using postmark templates
+
+### 🚧 Error Monitoring ([Sentry](https://sentry.io/welcome/)) 🚧
+
+TODO: based on [demand](https://github.com/devtodollars/flutter-supabase-production-template/issues/18).
---
+++ supabase/functions/stripe_webhook/index.ts
@@ -0,0 +1,125 @@
+import { processWebhookRequest } from "../_shared/stripe.ts";
+import { supabase } from "../_shared/supabase.ts";
+import { stripe } from "../_shared/stripe.ts";
+import Stripe from "stripe";
+import { posthog } from "../_shared/posthog.ts";
+import { sendEmail } from "../_shared/postmark.ts";
+
+Deno.serve(async (req) => {
+  let receivedEvent;
+  try {
+    receivedEvent = await processWebhookRequest(req);
+  } catch (e) {
+    return new Response(e.message, { status: 400 });
+  }
+
+  console.log(`🔔 Received event: ${receivedEvent.type}`);
+  const object = receivedEvent.data.object;
+  switch (receivedEvent.type) {
+    case "customer.subscription.deleted":
+      await onSubscriptionUpdated(object, true);
+      break;
+    case "customer.subscription.updated":
+      await onSubscriptionUpdated(object);
+      break;
+    case "customer.subscription.created":
+      await onSubscriptionUpdated(object);
+      break;
+    case "checkout.session.completed":
+      await onCheckoutComplete(object);
+      break;
+  }
+  return new Response(JSON.stringify({ ok: true }), { status: 200 });
+});
+
+async function onSubscriptionUpdated(
+  subscription: Stripe.Subscription,
+  deleted = false,
+) {
+  const prods = await getActiveProducts(subscription.customer);
+  const subscriptionItems = subscription.items.data;
+  const validStatuses = ["incomplete", "trialing", "active"];
+  for (const item of subscriptionItems) {
+    const prod = item.plan.product;
+
+    if (deleted || !validStatuses.includes(subscription.status)) {
+      // removes product from purchased products
+      const i = prods.indexOf(prod);
+      if (i !== -1) prods.splice(i, 1);
+    } else if (!prods.includes(prod)) {
+      prods.push(prod);
+    }
+  }
+  // updates purchased_products
+  const { data } = await supabase.from("stripe").update({
+    active_products: prods,
+  }).eq("stripe_customer_id", subscription.customer).select();
+  const row = data?.[0];
+
+  // posthog
+  if (row) {
+    const products = subscriptionItems.map((
+      item: { plan: { product: string } },
+    ) => item.plan.product);
+    posthog.capture({
+      distinctId: row.user_id,
+      event: "user completes checkout",
+      properties: {
+        products,
+        $set: {
+          "stripe_customer_id": row.stripe_customer_id,
+        },
+      },
+    });
+  }
+}
+
+async function onCheckoutComplete(session: Stripe.Session) {
+  const prods = await getActiveProducts(session.customer);
+  const { data: lineItems } = await stripe.checkout.sessions.listLineItems(
+    session.id,
+  );
+
+  for (const item of lineItems) {
+    const prod = item.price.product;
+    // skip if product is subscription or already purchased
+    if (item.mode === "subscription" || prods.includes(prod)) continue;
+    prods.push(prod);
+  }
+  await supabase.from("stripe").update({
+    active_products: prods,
+  }).eq("stripe_customer_id", session.customer);
+
+  // Sends email based on purchase
+  const checkoutProducts = lineItems.map((i: Stripe.LineItem) =>
+    i.price.product
+  );
+  await sendPurchaseEmail(checkoutProducts, session.customer_details.email);
+}
+
+async function getActiveProducts(customer: string): Promise<string[]> {
+  const { data } = await supabase.from("stripe").select(
+    "active_products",
+  ).eq(
+    "stripe_customer_id",
+    customer,
+  ).single();
+  const purchasedProds: string[] = data?.active_products || [];
+  return purchasedProds;
+}
+
+async function sendPurchaseEmail(products: string[], to: string) {
+  // TODO: update this function based on your emailing needs
+  const product = products[0];
+  let template = "";
+  if (product === "prod_PfRVCVqv8fBrxN") {
+    template = "paid-docs-support";
+  } else if (product === "prod_PfRUhnTnwtHLvw") {
+    template = "paid-docs";
+  } else if (product === "prod_PfRXymhiBMCUZo") {
+    template = "paid-consulting";
+  }
+  if (template) {
+    await sendEmail({ to, template });
+  }
+}
